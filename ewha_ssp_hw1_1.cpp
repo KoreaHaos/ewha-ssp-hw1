ed83a70ce818ef6480cb82d6b5396344f2808bae
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-03-28 01:02:37 +0000

    Modified setup and loop functions using code from lab one part one.

--- ewha_ssp_hw1_1.cpp
+++ ewha_ssp_hw1_1.cpp
@@ -10,15 +10,26 @@
  *
  */
 
-// Add #define pre-processor below
+/* Author: HyungJune Lee */
+/* Modifications by Benjamin Haos */
+/* Date: Mar 14, 2016 */
 
- 
-void setup() {
-  //Add your code of Lab Assignment #1
+#define FRONT_LED_PIN  10
+#define REAR_LED_PIN  9
 
+void setup()
+{
+  pinMode(FRONT_LED_PIN, OUTPUT);
+  pinMode(REAR_LED_PIN, OUTPUT);
 }
 
-void loop() {
-  //Add your code of Lab Assignment #1  
+void loop()
+{
+  digitalWrite(FRONT_LED_PIN, HIGH);
+  digitalWrite(REAR_LED_PIN, LOW);
+  delay(1000);
 
+  digitalWrite(FRONT_LED_PIN, LOW);
+  digitalWrite(REAR_LED_PIN, HIGH);
+  delay(1000);
 }

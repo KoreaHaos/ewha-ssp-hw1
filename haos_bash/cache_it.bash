1ce371246b5a03b816dc72f5c2022bbb73a1ca63
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-03-28 01:10:07 +0000

    Saved with haos_bash script <-- cmon' Ben!

---
+++ haos_bash/cache_it.bash
@@ -0,0 +1,2 @@
+# Make git rememember passwords for 30 minutes.
+git config --global credential.helper 'cache --timeout=1800'
---
+++ haos_bash/save_it.bash
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Saved with haos_bash script <-- cmon' Ben!"
+git push --all
